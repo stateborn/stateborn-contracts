e20b81d171888166e8b3ab83de65f34961ad759a
Przemyslaw Thomann <peerzet3@gmail.com>
2023-09-08 13:40:53 +0200

    Deployments after deploy to polygon

--- deploy/01-deploy-erc-20-dao-factory.ts
+++ deploy/01-deploy-erc-20-dao-factory.ts
@@ -1,33 +1,21 @@
-// TypeScript
-import { DeployFunction, DeployResult } from "hardhat-deploy/dist/types";
+import { DeployFunction } from "hardhat-deploy/dist/types";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { network } from "hardhat";
-import fs from 'fs';
 
 const deployErc20DaoFactory: DeployFunction = async (
     hre: HardhatRuntimeEnvironment
 ) => {
     const { deploy } = hre.deployments;
     const { deployer } = await hre.getNamedAccounts();
+    console.log('deployer', deployer);
     const chainId = network.config.chainId!;
-    const currentNetwork = network.name;
 
-    const deployerResult = await deploy("ERC20DaoFactory", {
+    await deploy("ERC20DaoFactory", {
         from: deployer,
         log: true,
         args: [],
         waitConfirmations: chainId == 31337 ? 1 : 6,
     });
-
-    if (!fs.existsSync('deployments')) {
-        fs.mkdirSync('deployments');
-    }
-    fs.writeFileSync(
-        `deployments/erc-20-dao-factory-${currentNetwork}.json`,
-        JSON.stringify({
-            address: deployerResult.address,
-        }, null, 2));
-    console.log('Done!');
 };
 
 export default deployErc20DaoFactory;
--- deploy/02-deploy-nft-dao-factory.ts
+++ deploy/02-deploy-nft-dao-factory.ts
@@ -1,8 +1,6 @@
-// TypeScript
-import { DeployFunction, DeployResult } from "hardhat-deploy/dist/types";
+import { DeployFunction } from "hardhat-deploy/dist/types";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { network } from "hardhat";
-import fs from 'fs';
 
 const deployNftDaoFactory: DeployFunction = async (
     hre: HardhatRuntimeEnvironment
@@ -10,23 +8,13 @@ const deployNftDaoFactory: DeployFunction = async (
     const { deploy } = hre.deployments;
     const { deployer } = await hre.getNamedAccounts();
     const chainId = network.config.chainId!;
-    const currentNetwork = network.name;
 
-    const deployerResult = await deploy("NFTDaoFactory", {
+    await deploy("NFTDaoFactory", {
         from: deployer,
         log: true,
         args: [],
         waitConfirmations: chainId == 31337 ? 1 : 6,
     });
-    if (!fs.existsSync('deployments')) {
-        fs.mkdirSync('deployments');
-    }
-    fs.writeFileSync(
-        `deployments/nft-dao-factory-${currentNetwork}.json`,
-        JSON.stringify({
-            address: deployerResult.address,
-        }, null, 2));
-    console.log('Done!');
 };
 
 export default deployNftDaoFactory;
